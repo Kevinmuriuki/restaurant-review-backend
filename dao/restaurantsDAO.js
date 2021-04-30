1c32f27e5954b98fef1b66132d44141471b84ac5
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2021-04-30 16:44:01 +0300

    added reviews controler and functionality to update atlas and other modifications

--- dao/restaurantsDAO.js
+++ dao/restaurantsDAO.js
@@ -1,3 +1,5 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectID
 let restaurants
 
 export default class RestaurantsDAO {
@@ -31,9 +33,10 @@ export default class RestaurantsDAO {
     }
 
     let cursor
-
+    
     try {
-      cursor = await restaurants.find(query)
+      cursor = await restaurants
+        .find(query)
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return { restaurantsList: [], totalNumRestaurants: 0 }
@@ -53,60 +56,58 @@ export default class RestaurantsDAO {
       return { restaurantsList: [], totalNumRestaurants: 0 }
     }
   }
-}
-//   }
-//   static async getRestaurantByID(id) {
-//     try {
-//       const pipeline = [
-//         {
-//             $match: {
-//                 _id: new ObjectId(id),
-//             },
-//         },
-//               {
-//                   $lookup: {
-//                       from: "reviews",
-//                       let: {
-//                           id: "$_id",
-//                       },
-//                       pipeline: [
-//                           {
-//                               $match: {
-//                                   $expr: {
-//                                       $eq: ["$restaurant_id", "$$id"],
-//                                   },
-//                               },
-//                           },
-//                           {
-//                               $sort: {
-//                                   date: -1,
-//                               },
-//                           },
-//                       ],
-//                       as: "reviews",
-//                   },
-//               },
-//               {
-//                   $addFields: {
-//                       reviews: "$reviews",
-//                   },
-//               },
-//           ]
-//       return await restaurants.aggregate(pipeline).next()
-//     } catch (e) {
-//       console.error(`Something went wrong in getRestaurantByID: ${e}`)
-//       throw e
-//     }
-//   }
+  static async getRestaurantByID(id) {
+    try {
+      const pipeline = [
+        {
+            $match: {
+                _id: new ObjectId(id),
+            },
+        },
+              {
+                  $lookup: {
+                      from: "reviews",
+                      let: {
+                          id: "$_id",
+                      },
+                      pipeline: [
+                          {
+                              $match: {
+                                  $expr: {
+                                      $eq: ["$restaurant_id", "$$id"],
+                                  },
+                              },
+                          },
+                          {
+                              $sort: {
+                                  date: -1,
+                              },
+                          },
+                      ],
+                      as: "reviews",
+                  },
+              },
+              {
+                  $addFields: {
+                      reviews: "$reviews",
+                  },
+              },
+          ]
+      return await restaurants.aggregate(pipeline).next()
+    } catch (e) {
+      console.error(`Something went wrong in getRestaurantByID: ${e}`)
+      throw e
+    }
+  }
 
-//   static async getCuisines() {
-//     let cuisines = []
-//     try {
-//       cuisines = await restaurants.distinct("cuisine")
-//       return cuisines
-//     } catch (e) {
-//       console.error(`Unable to get cuisines, ${e}`)
-//       return cuisines
-//     }
-//   }
-// }
+  static async getCuisines() {
+    let cuisines = []
+    try {
+      cuisines = await restaurants.distinct("cuisine")
+      return cuisines
+    } catch (e) {
+      console.error(`Unable to get cuisines, ${e}`)
+      return cuisines
+    }
+  }
+}
